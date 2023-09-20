c207ae9139dc44569e93340b09c5699df302e592
Hon Man Wong <honman-wong@hotmail.com>
2023-09-20 10:08:14 +0200

    initial commit

---
+++ src/main/java/com/createment/footballmanager/FootballManagerApplication.java
@@ -0,0 +1,13 @@
+package com.createment.footballmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/createment/footballmanager/FootballManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.createment.footballmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
